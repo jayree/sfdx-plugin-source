75ea60f9e3adeb76a81f676c46f8392698e4a398
jayree <jayree.git@icloud.com>
2024-07-27 15:55:04 +0200

    chore: fix

--- src/hooks/scopedPreRetrieve.ts
+++ src/hooks/scopedPreRetrieve.ts
@@ -23,7 +23,7 @@ type HookOptions = {
 // eslint-disable-next-line @typescript-eslint/require-await
 export const scopedPreRetrieve: HookFunction = async function (options) {
   const debug = Debug([this.config.bin, '@jayree/sfdx-plugin-source', 'hooks', 'scopedPreRetrieve'].join(':'));
-  debug(`called by: ${options.Command.id}`);
+  debug(`called by: ${options.Command.id as string}`);
 
   env.setBoolean('SFDX_DISABLE_PRETTIER', true);
   debug('set: SFDX_DISABLE_PRETTIER=true');
