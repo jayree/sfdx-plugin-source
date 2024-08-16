dbcb63bf08d37330e6d6e6561083be1b0da9fe37
jayree <jayree.git@icloud.com>
2024-08-16 13:58:13 +0200

    chore: fix

--- lib/commands/jayree/project/fix.d.ts
+++ lib/commands/jayree/project/fix.d.ts
@@ -4,7 +4,7 @@ export default class FixMetadata extends SfCommand<AnyJson> {
     static readonly summary: string;
     static readonly flags: {
         'target-org': import("@oclif/core/interfaces").OptionFlag<import("@salesforce/core").Org, import("@oclif/core/interfaces").CustomOptions>;
-        task: import("@oclif/core/interfaces").OptionFlag<string[], import("@oclif/core/interfaces").CustomOptions>;
+        task: import("@oclif/core/interfaces").OptionFlag<string[] | undefined, import("@oclif/core/interfaces").CustomOptions>;
     };
     static readonly requiresProject = true;
     static readonly deprecateAliases = true;
