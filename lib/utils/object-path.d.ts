46f9f2cb47112159d6cf5162a341683be9056270
jayree <jayree.git@icloud.com>
2025-11-16 11:20:48 +0100

    chore: fix

--- lib/utils/object-path.d.ts
+++ lib/utils/object-path.d.ts
@@ -1,10 +1,10 @@
 import objectPath from 'object-path';
 declare function compareobj<T extends object>(obj1: T | string, obj2: T | string): boolean;
-interface QueryParameters {
+type QueryParameters = {
     path: string;
     key?: string[];
     value?: string[] | string;
-}
+};
 declare class ObjectPathResolver {
     private path;
     private object;
